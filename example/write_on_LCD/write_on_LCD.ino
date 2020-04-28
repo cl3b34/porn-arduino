b3c15d5783a17aea829c448a2f0d974392db36ae
cl3b34 <cl3b34@gmail.com>
2020-04-28 07:41:52 +0300

    write on the big LCD (dev) first

--- example/write_on_LCD/write_on_LCD.ino
+++ example/write_on_LCD/write_on_LCD.ino
@@ -7,19 +7,23 @@
 
 void setup()
   {
-    lcd.begin(16, 2);                          // put your LCD parameters here
-    for (char i=47; i<127; i++)                // send 80 consecutive displayable characters to the LCD
-      {
-        lcd.print(i);
-        delay(100);                            // this delay allows you to observe the addressing sequence
-      }
-      
+
+   
     lcd1.begin(20, 4);                          // put your LCD parameters here
     for (char i=47; i<127; i++)                // send 80 consecutive displayable characters to the LCD
       {
         lcd1.print(i);
         delay(100);                            // this delay allows you to observe the addressing sequence
       }
+      
+    lcd.begin(16, 2);                          // put your LCD parameters here
+    for (char i=47; i<127; i++)                // send 80 consecutive displayable characters to the LCD
+      {
+        lcd.print(i);
+        delay(100);                            // this delay allows you to observe the addressing sequence
+      }
+
+
   }
 
 

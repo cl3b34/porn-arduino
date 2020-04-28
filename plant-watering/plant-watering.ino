3bbfb5fbfe4938b57b34b83221490578394e116e
cl3b34 <cl3b34@gmail.com>
2020-04-28 07:39:44 +0300

    Debug scaffolding, print uptime, fix ever growing error message

--- plant-watering/plant-watering.ino
+++ plant-watering/plant-watering.ino
@@ -6,19 +6,40 @@
 
 #include <LiquidCrystal.h>
 
+// Scaffolding for debug
+#define DEBUG   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
+#ifdef DEBUG    //Macros are usually in all capital letters.
+  #define DPRINT(...)    Serial.print(__VA_ARGS__)     //DPRINT is a macro, debug print
+  #define DPRINTLN(...)  Serial.println(__VA_ARGS__)   
+  #define DELAY(...)    delay(__VA_ARGS__)
+
+#else
+  #define DPRINT(...)     //now defines a blank line
+  #define DPRINTLN(...)  
+  #define DELAY(...)   
+#endif
+
+
+
 const int relayON = LOW;                  //  Our relay activates when it gets a LOW signal from arduino
 const int relayOFF = HIGH;                //  Our relay activates when it gets a LOW signal from arduino
 const int pumpPowerPin = 2;
 const int moistureSensorsPowerPin = 26;    // all moisture sensors are powered from the same pin
 const int waterLevelSensorPowerPin = 27; 
 const int waterLevelSensorPin = A15;
-//const int samplesToTake = 30;             // Take a few sample measurements and average them for better precision
-const int samplesToTake = 5;
-const int timeBetweenSamples =  300;
 long previousTime = 0;                   // The last time we run
 const long sleepTime = 8000000;            // Time between runs to check if plant need water. 4000000 = 66 minutes. 8.000.000 = 133 minutes (time is in miliseconds)
-const int lcdRows = 2;
-const int lcdCols = 16;
+#ifdef DEBUG 
+  const int samplesToTake = 5;             
+  const int timeBetweenSamples =  100;
+  const int lcdRows = 4;
+  const int lcdCols = 20;
+#else  // Development rig
+  const int samplesToTake = 30;             // Take a few sample measurements and average them for better precision
+  const int timeBetweenSamples =  300;
+  const int lcdRows = 2;
+  const int lcdCols = 16;
+#endif 
 
 /*
   The plant we are caring for, If the plant doesn't have a name, we will not look after her, so give her a name!
@@ -34,14 +55,22 @@ int solenoidPowerPin[] = {3, 4, 5 ,6 ,7 ,8 ,9 ,10 , 11, 12, 13, 22, 23, 24, 25};
    When defining a new plant, put a proper value here. 
    plant that like it wet should hover close to 500, dry should remain close to 1000 
 */
-//int startWatering[] = {575, 550, 525, 800, 550, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200};
-int startWatering[] = {249, 550, 525, 800, 550, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200};
-int stopWatering[] = {550, 525, 500, 775, 525, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200};       // When to stop watering  Be conservative, it is easy to get it too wet before the sensor measurement changes (water takes time to soak in)
-//int wateringTime[] = {15000, 8000, 15000, 30000, 30000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};                            // How long to water for ( ms )
-int wateringTime[] = {1000, 8000, 15000, 30000, 30000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};                            // How long to water for ( ms )
-int sensorSafetyUpperLimit = 850;  // Any measurement above this is considered a faulty sensor, broken or disconnected
-int sensorSafetyLowerLimit = 550;  // Any measurement bellow this is considered a faulty sensor, broken or disconnected
-int sensorSafetyShorted = 1; // A reading of 1 means the sensor is shorted
+#ifdef DEBUG 
+  int startWatering[] = {100, 600, 600, 800, 600, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200};  // first plant always is watering
+  int stopWatering[] = {600, 575, 575, 775, 575, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200};      
+  int wateringTime[] = {3000, 8000, 15000, 30000, 30000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};                            // water for just enough time so we can inspect
+  int sensorSafetyUpperLimit = 850;  
+  int sensorSafetyLowerLimit = 400;  // force at least one plant sensor to be 'defective'
+  int sensorSafetyShorted = 1; 
+#else  // Development
+  int startWatering[] = {625, 600, 600, 800, 600, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200};
+  int stopWatering[] = {600, 575, 575, 775, 575, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200, 1200};       // When to stop watering  Be conservative, it is easy to get it too wet before the sensor measurement changes (water takes time to soak in)
+  int wateringTime[] = {15000, 8000, 15000, 30000, 30000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};                            // How long to water for ( ms )
+  int sensorSafetyUpperLimit = 850;  // Any measurement above this is considered a faulty sensor, broken or disconnected
+  int sensorSafetyLowerLimit = 250;  // Any measurement bellow this is considered a faulty sensor, broken or disconnected
+  int sensorSafetyShorted = 1; // A reading of 1 means the sensor is shorted
+#endif
+
 // Flags to identify which plants need water
 boolean shouldWater[15] = {false};
 // Fautly sensors
@@ -57,10 +86,9 @@ void setup() {
   // start the serial and the LCD
   Serial.begin(9600);
   lcd.begin(lcdCols, lcdRows); 
-  Serial.println("Initializing...");
-  lcdWrite("Initializing...");   
-            
-  
+  DPRINTLN("Initializing...");
+  lcdWrite("Initializing...");
+
   for(byte i = 0; i < (sizeof(solenoidPowerPin) / sizeof(solenoidPowerPin[0])); i++) 
   {
       pinMode(solenoidPowerPin[i], OUTPUT);  
@@ -84,8 +112,6 @@ void setup() {
   * 
   * TODO: Connect a water level sensor to the water reservoir
   * 
-  * TODO: Connect a LCD screen to show the watering status: Last plants watered and measured moisture level.
-  *       Bonus: Temperature inside and outside, uptime
   */
 void loop() {
   unsigned long currentTime = millis();
@@ -100,7 +126,7 @@ void loop() {
       if( plant[i] != "" && wateringTime[i] > 0){
         // The moisture number is counter intuitive. A big number indicates dry ground
         int moistureAveraged = sampleMoisture(i);
-        Serial.println("Sampled " + plant[i] + ": " + moistureAveraged);
+        DPRINTLN("Sampled " + plant[i] + ": " + moistureAveraged);
         lcdWrite(plant[i] + ": " + moistureAveraged);
 
         // Safety checks
@@ -108,24 +134,23 @@ void loop() {
           // We got a broken sensor? A shorted connection? an empty tank? a leaky hose? in any case, will not water this one
           isBroken[i] = true;
           shouldWater[i] = false; 
-          Serial.println("Sensor on " + plant[i] +  " gave a reading out of range (" + moistureAveraged + "), please check if it is broken");
-          hasError = "Error:";
+          DPRINTLN("Sensor on " + plant[i] +  " gave a reading out of range (" + moistureAveraged + "), please check if it is broken");
         }
   
         if (moistureAveraged > startWatering[i] && ! isBroken[i]){
-          Serial.println("Moisture is low in " + plant[i] + " needs watering");
+          DPRINTLN("Moisture is low in " + plant[i] + " needs watering");
           // Set a flag for this plant, we will water all at the same time later
           shouldWater[i] = true; 
           lastWatered = "Last Watered: ";
         }else if (moistureAveraged < stopWatering[i]) {
-          Serial.println("Moisture is high in " + plant[i] + " do NOT need watering");
+          DPRINTLN("Moisture is high in " + plant[i] + " do NOT need watering");
           // remove the flag
           shouldWater[i] = false; 
         }
       }else{
-        Serial.print("No plant installed on sensor ");
-        Serial.print(i, DEC);
-        Serial.println(" . Skipping.");
+        DPRINT("No plant installed on sensor ");
+        DPRINT(i, DEC);
+        DPRINTLN(" . Skipping.");
         shouldWater[i] = false;
       }
     }
@@ -137,13 +162,13 @@ void loop() {
     for(byte i = 0; i < (sizeof(shouldWater) / sizeof(shouldWater[0])); i++) 
     {
       if (shouldWater[i] == true) {
-        Serial.println("Watering " + plant[i] + " for " + wateringTime[i]/1000 + "s");
+        DPRINTLN("Watering " + plant[i] + " for " + wateringTime[i]/1000 + "s");
         lcdWrite("Watering " + plant[i] + " for " + wateringTime[i]/1000 + "s");
         digitalWrite(solenoidPowerPin[i], relayON);           // First open valve
         digitalWrite(pumpPowerPin, relayON);                  // Then turn pump on
         delay(wateringTime[i]);                               // Water for the defined period
         digitalWrite(solenoidPowerPin[i], relayOFF); // Close the valve
-        Serial.println("adding " + plant[i] + " to last watered list");
+        DPRINTLN("adding " + plant[i] + " to last watered list");
         lastWatered += plant[i];
         lastWatered += " ";
       }
@@ -157,30 +182,46 @@ void loop() {
 
   if (lastWatered != ""){
     lcdWrite(lastWatered);
-    Serial.println(lastWatered);
+    DPRINTLN(lastWatered);
     delay(2000);
   }
   unsigned int timeLeft = (sleepTime - (currentTime - previousTime))/1000/60;
-  Serial.print("Minutes to next run: ");
-  Serial.println(timeLeft);
+  DPRINT("Minutes to next run: ");
+  DPRINTLN(timeLeft);
   lcd.clear();
   lcd.print("Next run: " );
   lcd.print(timeLeft);
   lcd.print("m");
   delay(5000);
 
+  // Time since last reset, in minutes
+  unsigned int uptime = currentTime/1000/60;
+  DPRINT("Uptime: ");
+  DPRINTLN(uptime);
+  lcd.clear();
+  lcd.print("Uptime: " );
+  lcd.print(uptime);
+  lcd.print("m");
+  delay(5000);
+
+  
   // Collect the errors for displaying
   for(byte i = 0; i < (sizeof(isBroken) / sizeof(isBroken[0])); i++) 
   {
-    if (isBroken[i] == true) {
-      hasError += " " + plant[i];
-    }
+    if(isBroken[i] == true){
+      if(hasError == ""){
+        hasError += "Error, these sensors gave an strange reading:";
+        hasError += " " + plant[i];
+      }else{
+        hasError += " " + plant[i];
+      }
+      isBroken[i] = false;
+    }      
   }
   if(hasError != ""){
-    hasError += " gave an strange value";
     lcdWrite(hasError);
-//      delay(20000);
   }
+//    delay(20000);
 }
 
 
@@ -195,9 +236,13 @@ int sampleMoisture(int moistureSensor)
   }
   // Return the average of the readings 
   return moistureSampleSum / samplesToTake;
- }
+}
 
- void lcdWrite(String text){
+/*
+ * Handles changes of line and pagination for different sizes of LCD Screens 
+ * Currently supports only 16x2 and 20x4
+ */
+void lcdWrite(String text){
   // A 2 lines LCD writes on line 0 and 2
   // A 4 lines will write on lines 0, 2, 1 and finally 3
   int lcdLines[lcdRows];
@@ -209,8 +254,9 @@ int sampleMoisture(int moistureSensor)
       lcdLines[i]=i;
     }
   }   
- Serial.print("Text size to print ");
- Serial.println(text.length());
+  
+  DPRINT("Text size to print ");
+  DPRINTLN(text.length());
 
   lcd.clear();
   if(text.length() > lcdCols){
@@ -218,52 +264,59 @@ int sampleMoisture(int moistureSensor)
     text.toCharArray(buffer, text.length()+1);
     byte lin = 0;
     for(byte i=0; i<text.length(); i++){
-//      Serial.print("calculating remainder of ");
-//      Serial.print( i+1);
-//      Serial.print(" and " );
-//      Serial.println((lcdCols * lcdRows)+1);
-      Serial.print("Remainder screen " );
-      Serial.println((i+1) % ((lcdCols * lcdRows)+1));
+//       DPRINT("calculating remainder of ");
+//       DPRINT( i+1);
+//       DPRINT(" and " );
+//       DPRINTLN(lcdCols * lcdRows);
+//       DPRINT("Remainder screen " );
+//       DPRINTLN((i+1) % (lcdCols * lcdRows));
 
-      if( (i+1) % ((lcdCols * lcdRows)+1) != 0){  // the screen is not full yet
-          if( (i+1) % (lcdCols+1)  != 0  ){  // the line is not full yet
-//            Serial.print("Remainder line of ");
-//            Serial.print( i+1);
-//            Serial.print(" and ");
-//            Serial.println(lcdCols+1);
-              Serial.print("Remainder line " );
-              Serial.println( (i+1) % (lcdCols+1) );
+      if( (i+1) % (lcdCols * lcdRows) != 0){    // the screen is not full yet
+          DPRINT("Remainder line of ");
+          DPRINT( i+1);
+          DPRINT(" and ");
+          DPRINTLN(lcdCols);
+          DPRINT("Remainder line " );
+          DPRINTLN( (i+1) % (lcdCols) );
+          if( (i+1) % (lcdCols)  != 0  ){         // the line is not full yet
             lcd.print(buffer[i]); 
-            Serial.print(buffer[i]);
+//            DPRINT(buffer[i]);
             delay(100);
           }else{
             int lines = (sizeof(lcdLines)/sizeof(lcdLines[0]))-1;
-            Serial.print("lines " );
-            Serial.println(lines);
+            DPRINT("lines " );
+            DPRINTLN(lines);
             if(lin < (sizeof(lcdLines)/sizeof(lcdLines[0]))-1){
+              DPRINTLN("adding to lin ");
               lin += 1;              
             }else{
+              DPRINTLN("rolling lin back to zero ");
               lin = 0; // back to first line
             }
-            Serial.print("line variable ");
-            Serial.println(lin);
-            Serial.print("Moving to next line: ");
-            Serial.println(lcdLines[lin]); 
-            for(byte x=0; x<sizeof(lcdLines)/sizeof(lcdLines[0]); x++){
-              Serial.print("contents of lcdLines ");
-              Serial.println(lcdLines[x]);
-            }
-            lcd.setCursor(0,lcdLines[lin]);  // move cursor to next line
+            DPRINT("lin variable ");
+            DPRINTLN(lin);
+            DPRINT("Moving to next line: ");
+            DPRINTLN(lcdLines[lin]); 
+//            for(byte x=0; x<sizeof(lcdLines)/sizeof(lcdLines[0]); x++){
+//              DPRINT("contents of lcdLines ");
+//              DPRINTLN(lcdLines[x]);
+//            }
+            DPRINT("Moving cursor to next line ");
+            DPRINTLN(lcdLines[lin]);
+            lcd.setCursor(0,lcdLines[lin]);         // move cursor to next line
             lcd.print(buffer[i]); 
-            Serial.print(buffer[i]);
+            DPRINT(buffer[i]);
             delay(100);
         }
       }else{  // move to next 'page'
-        Serial.println("Moving to next page");
+        DPRINTLN("Moving to next page");
         delay(2000);
         lcd.clear();
+        DPRINTLN("Cursor moved to line zero");
+        lin = 0;
+        DPRINTLN("rolling lin back to zero ");
         lcd.print(buffer[i]);
-        Serial.print(buffer[i]);
+        DPRINT(buffer[i]);
         delay(100);
       }
    }   
